735da635455d51a9a3e2b7b52e8d50229dac2917
supersonic-45 <130476833+supersonic-45@users.noreply.github.com>
2023-07-18 19:56:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -10,7 +16,13 @@ function setup() {
 }
 
 function draw() {
-    background('#008000');
+
+    background('#00FFFF');
+    document.getElementById("font_size").innerHTML = "Font size of the text will be = "+ difference+"px";
+    textSize(difference)
+    fill ('#FF0000');
+    text('Aryan', 50, 400)
+
 }
 
 function modelLoaded() {
@@ -19,8 +31,17 @@ function modelLoaded() {
 
 function gotPoses(results)
 {
-    if(results.lenght > 0)
+    if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = "+noseX +"noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+       rightWristX = results[0].pose.rightWrist.x;
+       difference = floor(leftWristX - rightWristX);
+
+       console.log("leftWristX = " + leftWristX +"rightWristX="+ rightWristX + "difference = " +difference);
     }
 }
+
